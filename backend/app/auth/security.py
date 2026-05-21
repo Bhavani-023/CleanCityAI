5b3919682ec905d2e195ace357b1da90eea5bca1
Bhavani-023 <bhavanisalguti@gmail.com>
2026-05-21 13:47:27 +0530

    Fixed bcrypt permanently

--- backend/app/auth/security.py
+++ backend/app/auth/security.py
@@ -1,23 +1,21 @@
 from passlib.context import CryptContext
 
 pwd_context = CryptContext(
-
     schemes=["bcrypt"],
-
     deprecated="auto"
-
 )
 
 def hash_password(password: str):
 
-    return pwd_context.hash(password[:72])
+    password = password[:72]
+
+    return pwd_context.hash(password)
 
 def verify_password(plain_password, hashed_password):
 
+    plain_password = plain_password[:72]
+
     return pwd_context.verify(
-
-        plain_password[:72],
-
+        plain_password,
         hashed_password
-
     )
