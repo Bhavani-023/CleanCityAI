4d7ecc95a08707750c35de2f47b51c14e7c442d8
Bhavani-023 <bhavanisalguti@gmail.com>
2026-05-21 14:17:51 +0530

    Added login API

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -8,39 +8,47 @@ from app.auth.security import hash_password
 
 router = APIRouter()
 
+# =========================
+# REGISTER
+# =========================
 @router.post("/register")
 def register(user: UserCreate, db: Session = Depends(get_db)):
 
-    # CHECK EXISTING USER
     existing_user = db.query(User).filter(User.email == user.email).first()
 
     if existing_user:
-
         raise HTTPException(
-
             status_code=400,
-
             detail="Email already registered"
-
         )
 
-    # CREATE NEW USER
     new_user = User(
-
         email=user.email,
-
         password=hash_password(user.password)
-
     )
 
     db.add(new_user)
-
     db.commit()
-
     db.refresh(new_user)
 
     return {
-
         "message": "User created successfully"
+    }
 
+# =========================
+# LOGIN
+# =========================
+@router.post("/login")
+def login(user: UserCreate, db: Session = Depends(get_db)):
+
+    existing_user = db.query(User).filter(User.email == user.email).first()
+
+    if not existing_user:
+        raise HTTPException(
+            status_code=404,
+            detail="User not found"
+        )
+
+    return {
+        "message": "Login successful"
     }
