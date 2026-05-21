be79e95f8052d7c1e5180f7a9726730e6717250d
Bhavani-023 <bhavanisalguti@gmail.com>
2026-05-21 14:33:34 +0530

    fixed app crash

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,43 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Register from "./pages/Register";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
+import { Toaster } from "react-hot-toast";
+
 import Landing from "./pages/Landing";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
+
   return (
+
     <BrowserRouter>
+
+      {/* TOAST */}
+
+      <Toaster
+        position="top-right"
+        reverseOrder={false}
+      />
+
+      {/* ROUTES */}
+
       <Routes>
 
         <Route path="/" element={<Landing />} />
 
-        <Route path="/register" element={<Register />} />
-
         <Route path="/login" element={<Login />} />
+
+        <Route path="/register" element={<Register />} />
 
         <Route path="/dashboard" element={<Dashboard />} />
 
       </Routes>
+
     </BrowserRouter>
+
   );
+
 }
 
 export default App;
