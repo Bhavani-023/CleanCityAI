d725e3912c31b63b1453137cb3021e7309d8b743
Bhavani-023 <bhavanisalguti@gmail.com>
2026-05-21 17:29:03 +0530

    fixed complaint routes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,30 +1,84 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+
+import os
 
 from app.database import engine
-from app.models.user import User
 
-from app.routes import auth
+from app.models.user import User
+from app.models.complaint import Complaint
+
+from app.routes import auth, complaint
+
+# =========================
+# FASTAPI APP
+# =========================
 
 app = FastAPI()
 
-# CREATE TABLES
-User.metadata.create_all(bind=engine)
+# =========================
+# CREATE DATABASE TABLES
+# =========================
 
+User.metadata.create_all(bind=engine)
+Complaint.metadata.create_all(bind=engine)
+
+# =========================
 # CORS
+# =========================
+
 app.add_middleware(
+
     CORSMiddleware,
+
     allow_origins=["*"],
+
     allow_credentials=True,
+
     allow_methods=["*"],
+
     allow_headers=["*"],
+
 )
 
+# =========================
 # ROUTES
+# =========================
+
 app.include_router(auth.router)
+
+app.include_router(complaint.router)
+
+# =========================
+# UPLOADS FOLDER
+# =========================
+
+os.makedirs("uploads", exist_ok=True)
+
+# =========================
+# SERVE IMAGES
+# =========================
+
+app.mount(
+
+    "/uploads",
+
+    StaticFiles(directory="uploads"),
+
+    name="uploads"
+
+)
+
+# =========================
+# HOME ROUTE
+# =========================
 
 @app.get("/")
 def home():
+
     return {
+
         "message": "Backend Running"
+
     }
