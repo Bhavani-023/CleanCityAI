50c80cef4a67a3deed85df172aafe16eb0849498
Bhavani-023 <bhavanisalguti@gmail.com>
2026-05-21 14:01:40 +0530

    Fixed routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Register from "./pages/Register";
-import Landing from "./pages/Landing";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
-import { Toaster } from "react-hot-toast";
+import Landing from "./pages/Landing";
+
 function App() {
   return (
-
     <BrowserRouter>
-    <Toaster position="top-right" />
-
       <Routes>
 
         <Route path="/" element={<Landing />} />
+
         <Route path="/register" element={<Register />} />
+
         <Route path="/login" element={<Login />} />
 
         <Route path="/dashboard" element={<Dashboard />} />
 
       </Routes>
-
     </BrowserRouter>
   );
 }
