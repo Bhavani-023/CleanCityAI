0b3c4cf1c05b286ddb093efa9428a86fb68e8826
Bhavani-023 <bhavanisalguti@gmail.com>
2026-05-23 13:23:30 +0530

    Restored topbar

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 
 import Sidebar from "../components/Sidebar";
+import Topbar from "../components/Topbar";
 
 import toast from "react-hot-toast";
 
@@ -16,15 +17,29 @@ export default function Dashboard() {
 
     <div className="min-h-screen bg-[#050816] text-white overflow-x-hidden">
 
+      {/* SIDEBAR */}
+
       <Sidebar />
 
-      <div className="ml-0 md:ml-[220px] flex items-center justify-center min-h-screen">
+      {/* MAIN */}
 
-        <h1 className="text-5xl font-bold text-cyan-400">
+      <div className="ml-0 md:ml-[220px] px-4 md:px-10 py-6">
 
-          Dashboard Working ✅
+        {/* TOPBAR */}
 
-        </h1>
+        <Topbar />
+
+        {/* CENTER */}
+
+        <div className="flex items-center justify-center mt-40">
+
+          <h1 className="text-5xl font-bold text-cyan-400">
+
+            Dashboard Working ✅
+
+          </h1>
+
+        </div>
 
       </div>
 
